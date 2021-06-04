3827d4e94cde3dbe43bb49752fddf4fe195b6afa
Błażej Sowa <bsowa123@gmail.com>
2021-06-04 20:13:57 +0200

    Reformat launch files with black

--- leo_viz/launch/rviz.launch.py
+++ leo_viz/launch/rviz.launch.py
@@ -6,28 +6,31 @@ from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
-    pkg_share = get_package_share_directory('leo_viz')
+    pkg_share = get_package_share_directory("leo_viz")
 
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            name='config',
-            default_value='robot',
-            description="The name of the cofig to use with RViz",
-        ),
-        DeclareLaunchArgument(
-            name='rviz_file',
-            default_value=[
-                pkg_share, '/rviz/', LaunchConfiguration('config'), '.rviz'
-            ],
-            description="Path to the config file for RViz",
-        ),
-        Node(
-            name='rviz2',
-            package='rviz2',
-            executable='rviz2',
-            output='screen',
-            arguments=[
-                '-d', LaunchConfiguration('rviz_file')
-            ],
-        )
-    ])
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                name="config",
+                default_value="robot",
+                description="The name of the config to use with RViz",
+            ),
+            DeclareLaunchArgument(
+                name="rviz_file",
+                default_value=[
+                    pkg_share,
+                    "/rviz/",
+                    LaunchConfiguration("config"),
+                    ".rviz",
+                ],
+                description="Path to the config file for RViz",
+            ),
+            Node(
+                name="rviz2",
+                package="rviz2",
+                executable="rviz2",
+                output="screen",
+                arguments=["-d", LaunchConfiguration("rviz_file")],
+            ),
+        ]
+    )
--- leo_viz/launch/view_model.launch.py
+++ leo_viz/launch/view_model.launch.py
@@ -8,54 +8,53 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    pkg_share = get_package_share_directory('leo_viz')
-    leo_description_share = get_package_share_directory('leo_description')
+    pkg_share = get_package_share_directory("leo_viz")
+    leo_description_share = get_package_share_directory("leo_description")
 
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            name='gui',
-            default_value='true',
-            description='Start joint_state_publisher GUI',
-        ),
-        DeclareLaunchArgument(
-            name='model',
-            default_value=leo_description_share + '/urdf/leo.urdf.xacro',
-            description='Absolute path to robot urdf.xacro file',
-        ),
-        Node(
-            name='robot_state_publisher',
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            output='screen',
-            parameters=[{
-                'robot_description': Command(['xacro ', LaunchConfiguration('model')])
-            }],
-        ),
-        Node(
-            condition=UnlessCondition(LaunchConfiguration('gui')),
-            name='joint_state_publisher',
-            package='joint_state_publisher',
-            executable='joint_state_publisher',
-            output='screen',
-            parameters=[{
-                'publish_default_efforts': True
-            }],
-        ),
-        Node(
-            condition=IfCondition(LaunchConfiguration('gui')),
-            name='joint_state_publisher',
-            package='joint_state_publisher_gui',
-            executable='joint_state_publisher_gui',
-            output='screen',
-            parameters=[{
-                'publish_default_efforts': True
-            }],
-        ),
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                pkg_share + '/launch/rviz.launch.py'),
-            launch_arguments=[
-                ('config', 'robot')
-            ],
-        )
-    ])
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                name="gui",
+                default_value="true",
+                description="Start joint_state_publisher GUI",
+            ),
+            DeclareLaunchArgument(
+                name="model",
+                default_value=[leo_description_share, "/urdf/leo.urdf.xacro"],
+                description="Absolute path to robot urdf.xacro file",
+            ),
+            Node(
+                name="robot_state_publisher",
+                package="robot_state_publisher",
+                executable="robot_state_publisher",
+                output="screen",
+                parameters=[
+                    {
+                        "robot_description": Command(
+                            ["xacro ", LaunchConfiguration("model")]
+                        )
+                    }
+                ],
+            ),
+            Node(
+                condition=UnlessCondition(LaunchConfiguration("gui")),
+                name="joint_state_publisher",
+                package="joint_state_publisher",
+                executable="joint_state_publisher",
+                output="screen",
+                parameters=[{"publish_default_efforts": True}],
+            ),
+            Node(
+                condition=IfCondition(LaunchConfiguration("gui")),
+                name="joint_state_publisher",
+                package="joint_state_publisher_gui",
+                executable="joint_state_publisher_gui",
+                output="screen",
+                parameters=[{"publish_default_efforts": True}],
+            ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([pkg_share, "/launch/rviz.launch.py"]),
+                launch_arguments={"config": "robot"}.items(),
+            ),
+        ]
+    )
